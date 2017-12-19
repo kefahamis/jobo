d0a283b16b80e0029b375651df46ac4082a30f79
kefahamis <hamiskf@gmail.com>
2017-12-19 19:10:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>campustalent</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		
+<div class="container-fluid">
+	       <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img class="logo" src="img/logo1.png" alt=""/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="nav navbar-nav navbar-left nav1">
+             <li class="active"><a href="#">Home</a></li>
+             <li><a href="aboutus.php">About Us</a></li>
+           </ul>
+             <ul class="nav navbar-nav navbar-left">
+              <li class="logbtn"><a type="button"  data-toggle="modal" data-target="#myModal"><i class="fa fa-user logicon" aria-hidden="true"></i>Login</a></li>
+    </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+</div>
+
+</div>
+<div class="over"></div>
+      <div class="row back">
+
+
+                  <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 txt">
+                   
+                         <p>Search your dream<span>Company </span></p>
+                         <input type="text"placeholder="What are you looking for" ><button>search<i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+
+                </div>
+   
+</div>
+<div class="container college">
+  <div class="row "> 
+    <div class="col-md-12">
+      <center><h6>top colleges</h6></center>
+    </div>
+  </div>
+  <div class="row first">
+    <div class="col-md-2 pic1">
+      <img src="img/1.png" class="img-responsive">
+    </div>
+        <div class="col-md-2 pic2">
+            <img src="img/2.png" class="img-responsive">
+        </div>
+            <div class="col-md-2 pic3">
+                <img src="img/3.png" class="img-responsive">
+            </div>
+                <div class="col-md-2 pic4">
+                    <img src="img/4.png" class="img-responsive">
+                </div>
+                    <div class="col-md-2 pic5">
+                        <img src="img/5.png" class="img-responsive">
+                    </div>
+                        <div class="col-md-2 pic6">
+                            <img src="img/6.png" class="img-responsive">
+                        </div>
+  </div>
+
+<!-- 2nd companies -->
+  <div class="row second">
+    <div class="col-md-12">
+      <center><h6>top companies</h6></center>
+    </div>
+  </div>
+    <div class="row">
+    <div class="col-md-2 pic7">
+      <img src="img/c1.png" class="img-responsive">
+    </div>
+        <div class="col-md-2 pic8">
+            <img src="img/c2.png" class="img-responsive">
+        </div>
+            <div class="col-md-2 pic9">
+                <img src="img/c3.png" class="img-responsive">
+            </div>
+                <div class="col-md-2 pic10">
+                    <img src="img/c4.png" class="img-responsive">
+                </div>
+                    <div class="col-md-2 pic11">
+                        <img src="img/c5.png" class="img-responsive">
+                    </div>
+                        <div class="col-md-2 pic12">
+                            <img src="img/c6.png" class="img-responsive">
+                        </div>
+  </div>
+</div>
+</div>
+<!-- footer -->
+<div class="container-fluid footer">
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 logo2">
+      <div class="col-md-12">
+        <img src="img/logo2.png">
+      </div>
+      <div class="col-sm-12 cy">Copyright © 2016 Campus Talent</div>
+      <div class="col-sm-12 sm"><i class="fa fa-facebook-official" aria-hidden="true"></i>
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                    <i class="fa fa-google-plus" aria-hidden="true"></i>
+                    <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                    <i class="fa fa-youtube" aria-hidden="true"></i></div>
+    </div>
+     <div class="col-md-4 des">
+       <div class="col-sm-12 au"><h1>About US</h1></div>
+       <div class="col-sm-12 "><p>Welcome to Campus Talent. We provide a platform for
+        the students, educators, and companies to meet and share collaboratively and benefit mutually. Currently, there is no medium of
+         connecting students to educators, educators to corporates, and corporates to students. Hence, either the students get enrolled in
+          any college and choose any course that is available and a bright talent may be lost. The corporates have to spend a big budget in
+           locating the best of the fresh talent and do not get what they had envisioned. 
+           The colleges too are in counselling about ... </p><a href="aboutus.php">Know more</a></div>
+     </div>
+      <div class="col-md-4 links">
+        <div class="col-sm-12 au"><h1>Quick Links</h1></div>
+        <div class="col-sm-12 ">
+                        <div class="qll">
+                  <div class=""><a href="index.php">Home</a></div>
+                  <div class=""><a href="aboutus.php">About us</a></div>
+                  <div class=""><a href="#">Contact Us</a></div>
+                  <div class=""><a href="wearehiring.php">We're hiring</a></div>
+                  <div class=""><a href="index.php">Blog</a></div>
+                  <div class=""><a href="index.php">Terms &amp; condition</a></div>
+                  
+                  
+                     </div>
+                    </div>
+      </div>
+  </div>
+  </div>
+</div>
+
+		<script src="js/jquery-3.2.1.min.js"></script>
+
+		<script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
